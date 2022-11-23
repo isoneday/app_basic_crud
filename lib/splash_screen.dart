844ec699058f676b4e7c3cd1525d6f462ae47abd
isoneday <iswandi.saputra11@gmail.com>
2022-11-23 09:38:55 +0700

    splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/home_screen.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
 
+//membuat halaman splash
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
@@ -9,12 +10,12 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SplashScreenView(
-      navigateRoute: HomeScreen(),
-      duration: 3000,
-      imageSize: 130,
-      imageSrc: "asset/images/logo.png",
-      backgroundColor: Colors.white,
-    ),
+        navigateRoute: HomeScreen(),
+        duration: 3000,
+        imageSize: 130,
+        imageSrc: "asset/images/logo.png",
+        backgroundColor: Colors.white,
+      ),
     );
   }
 }
